b4b83d8a218b316ec664f48b3798c304127070bf
Fanky <11221021@student.itk.ac.id>
2023-05-21 21:17:53 +0800

    restore App.java

--- Java/Sistem Diagnosa Penyakit/src/App.java
+++ Java/Sistem Diagnosa Penyakit/src/App.java
@@ -1,19 +1,5 @@
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 public class App {
-    public static void main(String[] args) {
-        Connection connection = mysql_connection.getConnection();
-
-        try {
-            System.out.println("Koneksi berhasil!");
-            connection.close();
-        } catch (SQLException e) {
-            System.out.println("Koneksi gagal!");
-            e.printStackTrace();
-        }
-
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
     }
 }
