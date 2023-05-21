c0f1326ec1ce02faeb7d0cd5dd319648bdcd55a6
Fanky <11221021@student.itk.ac.id>
2023-05-21 21:14:45 +0800

    menambahkan kode untuk melakukan koneksi dengan database

--- Java/Sistem Diagnosa Penyakit/src/App.java
+++ Java/Sistem Diagnosa Penyakit/src/App.java
@@ -1,5 +1,19 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Connection connection = mysql_connection.getConnection();
+
+        try {
+            System.out.println("Koneksi berhasil!");
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println("Koneksi gagal!");
+            e.printStackTrace();
+        }
+
     }
 }
