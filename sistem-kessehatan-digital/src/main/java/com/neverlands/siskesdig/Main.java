5d72ef7665286f1c00041ab305e425b1916d469b
Fanky21 <11221021@student.itk.ac.id>
2023-06-18 01:54:52 +0800

    add mysql database and connecton

--- sistem-kessehatan-digital/src/main/java/com/neverlands/siskesdig/Main.java
+++ sistem-kessehatan-digital/src/main/java/com/neverlands/siskesdig/Main.java
@@ -1,16 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.neverlands.siskesdig;
 import java.util.Scanner;
+import java.sql.*;
 
-/**
- *
- * @author Fakhrizal
- */
 public class Main {
+
+    static final String mysql_url = "jdbc:mysql://51.161.134.32/sistem_kesehatan";
+    static final String username = "database_pbo";
+    static final String password = "pbo331";
+
     public static void main(String[] args) throws Exception {
+        
         System.out.println("Selamat Datang!\n"
                 + "Pilih Menu:\n"
                 + "1. Login\n"
@@ -19,6 +18,61 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         
         String pilihan = scanner.nextLine();
-        System.out.println("Pilihan anda: " + pilihan);
+        
+        if (pilihan.equals("1")) {
+
+            System.out.println("Selamat datang diprogram Sistem kesehatan digital terpadu! \n"
+            + "Silahkan login menggunakan akun yang ada!");
+
+            String inputUsername = scanner.nextLine();
+
+            System.out.println("Silahkan masukkan password yang ada!");
+
+            String inputPassword = scanner.nextLine();
+
+            System.out.println("Anda memasukkan data sebagai berikut \n" 
+            + "Username anda: " + inputUsername + "\n"
+            + "Password anda: " + inputPassword + "\n"
+            + "Silahkan konfirmasi dengan tekan 1 dan tekan 2 untuk cancel!");
+
+            String confirmation = scanner.nextLine();
+
+            if (confirmation.equals("1")) {
+
+                System.out.println("Preparing connection");
+
+                Connection conn = DriverManager.getConnection(mysql_url, username, password);
+
+                System.out.println("Connection success!");
+
+                PreparedStatement syntax = conn.prepareStatement("SELECT * FROM akun WHERE username = ? AND password = ?");
+                syntax.setString(1, inputUsername);
+                syntax.setString(2, inputPassword);
+
+                ResultSet hasil = syntax.executeQuery();
+
+                Boolean status_login = hasil.next();
+
+                if (status_login) {
+
+                    System.out.println("Selamat datang " + inputUsername);
+
+                    System.out.println("Login berhasil");
+
+                } else {
+                    
+                    System.out.println("Login gagal");
+                }
+
+            } else if (confirmation.equals("2")) {
+
+                System.out.println("Cancel");
+
+            }
+            
+        } else if (pilihan.equals("2")) {
+
+        }
+
     }
 }
--- sistem-kessehatan-digital/src/main/java/com/neverlands/siskesdig/SistemKessehatanDigital.java
+++ sistem-kessehatan-digital/src/main/java/com/neverlands/siskesdig/SistemKessehatanDigital.java
@@ -4,13 +4,28 @@
 
 package com.neverlands.siskesdig;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Zenith
  */
 public class SistemKessehatanDigital {
 
+    private static final String url = "jdbc:mysql://51.161.134.32/sistem_kesehatan";
+    private static final String username = "database_pbo";
+    private static final String password = "pbo331";
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Koneksi berhasil!");
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println("Koneksi gagal!");
+            e.printStackTrace();
+        }
     }
 }
