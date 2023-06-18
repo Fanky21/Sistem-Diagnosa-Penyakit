42de2b55976030eb5c34c66e324f3775b2d15dfa
Fanky21 <11221021@student.itk.ac.id>
2023-06-18 22:08:50 +0800

    fixing DiagnosaPenyakit.java

--- sistem-kessehatan-digital/src/main/java/com/neverlands/siskesdig/DiagnosaPenyakit.java
+++ sistem-kessehatan-digital/src/main/java/com/neverlands/siskesdig/DiagnosaPenyakit.java
@@ -1,10 +1,14 @@
 package com.neverlands.siskesdig;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class DiagnosaPenyakit {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        Connection conn = DriverManager.getConnection(Main.mysql_url, Main.username, Main.password);
 
         Scanner scan = new Scanner(System.in);
 
@@ -12,8 +16,8 @@ public class DiagnosaPenyakit {
         System.out.print("Keluhan/Gejala: ");
         String sentence = scan.nextLine();
 
-        String filterWords = "sakit, makan, diare";
-        String[] filterArray = filterWords.split(",\\s*");
+        String gejala = "Radang,Bersin,Sesak,Nyeri sendi,Kelelahan,Demam,Penurunan berat badan,Sakit kepala,Muntah,lemas,Sesak napas,Berat badan menurun,Lemas,kaku,Kejang,Mudah lelah,Penglihatan kabur,Gangguan penglihatan,Mual,muntah,Sulit berkonsentrasi,Diare,Pusing,Pingsan,Nyeri,panas,Hilang nafsu makan,Linglung,Lemah otot,Penurunan kesadaran,Mata merah,Mata bengkak,Sakit tenggorokan,Jantung berdebar,Nyeri dada,Mual,Tubuh mudah lelah,Pembengkakan,Keringat dingin";
+        String[] filterArray = gejala.split(",\\s*");
         StringBuilder filteredWords = new StringBuilder();
 
         for (String word : filterArray) {
@@ -26,9 +30,73 @@ public class DiagnosaPenyakit {
             }
         }
 
-        String output = filteredWords.toString();
+        String output_data = filteredWords.toString();
 
+        System.out.println(output_data);
 
+        StringBuilder queryBuilder = new StringBuilder("SELECT nama_penyakit FROM penyakit WHERE");
+        List<String> placeholders = new ArrayList<>();
+
+        String[] gejalaArray = output_data.split(",\\s*");
+        for (int i = 0; i < gejalaArray.length; i++) {
+            String placeholder = "gejala_penyakit LIKE ?";
+            placeholders.add("%" + gejalaArray[i] + "%");
+
+            if (i > 0) {
+                queryBuilder.append(" AND");
+            }
+            queryBuilder.append(" ").append(placeholder);
+        }
+
+        String query = queryBuilder.toString();
+        PreparedStatement syntax = conn.prepareStatement(query);
+        for (int i = 0; i < placeholders.size(); i++) {
+            syntax.setString(i + 1, placeholders.get(i));
+        }
+
+        ResultSet hasil = syntax.executeQuery();
+        boolean status_hasil = hasil.next();
+
+        System.out.println(status_hasil);
+
+        if (status_hasil) {
+
+            System.out.println("Proses!");
+
+            List<String> penyakitList = new ArrayList<>();
+
+            while (hasil.next()) {
+                String data = hasil.getString("nama_penyakit");
+                penyakitList.add(data);
+            }
+
+            int counter = penyakitList.size();
+
+            if (counter > 3) {
+
+                System.out.println("Mohon masukkan gejala yang lebih spesifik!");
+
+            } else if (counter <= 3) {
+
+                System.out.println("Penyakit kamu adalah: ");
+
+                if (counter == 0) {
+
+                    System.out.println("Tidak ada penyakit yang cocok dengan gejala yang diberikan.");
+
+                } else {
+                    
+                    for (String penyakit : penyakitList) {
+                        System.out.println(penyakit);
+                    }
+                }
+            }
+
+        } else {
+
+            System.out.println("Data Penyakit Tidak Ditemukan!");
+
+        }
 
     }
     
