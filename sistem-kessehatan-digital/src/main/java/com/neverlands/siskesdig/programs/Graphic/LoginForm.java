0ff25378e2e8e8cea99308133fd9b99d055283de
Fakhrizal Naufal <11221057@student.itk.ac.id>
2023-06-19 15:26:11 +0800

    modify GUI

--- sistem-kessehatan-digital/src/main/java/com/neverlands/siskesdig/programs/Graphic/LoginForm.java
+++ sistem-kessehatan-digital/src/main/java/com/neverlands/siskesdig/programs/Graphic/LoginForm.java
@@ -5,16 +5,9 @@
 package com.neverlands.siskesdig.programs.Graphic;
 
 import java.awt.Color;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import javax.swing.JOptionPane;
-
 import com.neverlands.siskesdig.programs.LoginRegist;
-import com.neverlands.siskesdig.programs.controller.config;
 
 /**
  *
